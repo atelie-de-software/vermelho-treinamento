a1bccff730c8663290bd1cc7c46dffe00dcaf0ad
William <william@webgoal.com.br>
2019-09-20 10:12:06 -0300

    Beautify

--- main.rb
+++ main.rb
@@ -2,23 +2,24 @@ require 'dispel'
 
 x = 0
 y = 0
-output = ""
+output = ''
 # draw app and redraw after each keystroke
 Dispel::Screen.open do |screen|
-    Dispel::Keyboard.output timeout: 0.5 do |key|
-      screen.draw output
-      next unless key
-      exit(true) if key == :"Ctrl+c"
+  Dispel::Keyboard.output timeout: 0.5 do |key|
+    screen.draw output
+    next unless key
 
+    exit(true) if key == :"Ctrl+c"
 
-      x += 1 if key == :right
-      x -= 1 if key == :left
-      y += 1 if key == :up
-      y -= 1 if key == :down
+    x += 1 if key == :right
+    x -= 1 if key == :left
+    y += 1 if key == :up
+    y -= 1 if key == :down
 
-      output = "The time is #{Time.now}\n"
-      next if key == :timeout
-      output += "You pressed #{key}\n"
-      output += "x: #{x} y: #{y}\n"
-    end
+    output = "The time is #{Time.now}\n"
+    next if key == :timeout
+
+    output += "You pressed #{key}\n"
+    output += "x: #{x} y: #{y}\n"
+  end
 end
